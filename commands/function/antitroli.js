3486a1335327e60f0b9b8ab1b9de71e7971e83d3
Huihui <74879576+prioprast@users.noreply.github.com>
2021-11-26 20:38:01 +0700

    Update antitroli.js

--- commands/function/antitroli.js
+++ commands/function/antitroli.js
@@ -2,7 +2,7 @@ module.exports = {
 name: "antitroli",  
 async functions(m) { 
 if (m.message && m.quoted && m.quoted.mtype === 'orderMessage' && !(m.quoted.token && m.quoted.orderId)) {
-m.reply('Fake Troli Detected\n\n' + require('util').format(m.key))
+m.reply('Troli Detected\n\n' + require('util').format(m.key))
 }
 }
 }

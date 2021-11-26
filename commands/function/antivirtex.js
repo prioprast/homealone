81be57ae89936ce10dbde632fa7c569ea81bed72
Huihui <74879576+prioprast@users.noreply.github.com>
2021-11-26 20:39:15 +0700

    Update antivirtex.js

--- commands/function/antivirtex.js
+++ commands/function/antivirtex.js
@@ -7,7 +7,7 @@ async functions(m) {
 let { conn } = data
  if (m.text && m.text.length >= 15000) {
  if (m.sender == this.user.jid) return false
-let rojak = "\n".repeat(700) + "2020 - 2021 clear chat by Arifi Razzaq"
+let rojak = "\n".repeat(700) + "2020 - 2021 clear chat"
 m.reply(`Maaf @${m.sender.split("@")[0]} *Virtex Terdeteksi!*\n_Maaf Anda Di Kick._`)
 //await conn.groupRemove(m.chat, [m.sender])/terlalu mengerikan nanti dibanned
 m.reply(rojak)
